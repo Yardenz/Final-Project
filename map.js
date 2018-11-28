412deceffbf2827636a8c0b6b38ba5a6c48c877d
brianhouse <brian.house@gmail.com>
2018-11-28 16:24:31 -0500

    brian changes

--- map.js
+++ map.js
@@ -2,37 +2,69 @@
 
 console.log('Loaded map.js')
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieWFyZGVueiIsImEiOiJjam5hejlmMGkwMWV6M3BsOWJxcHNhZDJpIn0.yUUMMk4ixslNceyxLrAwzg'
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFyZGVueiIsImEiOiJjam5hejlmMGkwMWV6M3BsOWJxcHNhZDJpIn0.yUUMMk4ixslNceyxLrAwzg';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/yardenz/cjp1b65uf10z22sntfg71uhxv',
+center: [-111.860474, 40.760394],
+zoom: 12.5
+});
 
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/yardenz/cjp1b65uf10z22sntfg71uhxv',
-    center: [-73.96216,40.80779],
-    zoom: 16
+
+// console.log(map)
+
+map.on('load', function() {             // 'load' event handler
+
+    // console.log('developments-1dhpe2 copy')
+    // let features = map.queryRenderedFeatures({ layers: ['developments-1dhpe2 copy'] })
+    // for (let feature of features) {
+    //     console.log(feature)
+    // }
+
+    // console.log('developments-1dhpe2 copy 1')
+    // features = map.queryRenderedFeatures({ layers: ['developments-1dhpe2 copy 1'] })
+    // for (let feature of features) {
+    //     console.log(feature)
+    // }
+
 })
 
-let navigation = new mapboxgl.NavigationControl({
-    showCompass: false
-})
-map.addControl(navigation, 'top-left')
+let buildings = ["Gateway plaza", "Gateway Outline", "Olympic Village: Fort Douglas"]
 
-let scale = new mapboxgl.ScaleControl({
-    maxWidth: 80,
-    unit: 'imperial'
-})
-map.addControl(scale, 'bottom-right')
+let content = { "Gateway plaza": "this is some HTML<br /><img src='images/skier-big-mountain.jpg' width='100' height='100' /><br /><img src='https://www.telegraph.co.uk/content/dam/Travel/ski/skier-big-mountain.jpg' width='100' height='100' />",
+                "Gateway Outline": "more HTML",
 
-let geolocate = new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true,
-    showUserLocation: true,
-    fitBoundsOptions: {
+                }
+
+
+map.on('click', 'developments-1dhpe2 copy', function (e) {
+    for (let building of e.features) {
+        console.log(building.properties.NAME)
+        for (let building_name of buildings) {
+            if (building_name == building.properties.NAME) {
+                 new mapboxgl.Popup()
+                     .setLngLat(e.lngLat)
+                     .setHTML(content[building.properties.NAME])
+                     .addTo(map)
+                break
+            }
+        }
     }
-})
-map.addControl(geolocate, 'top-left')
+ })
 
-geolocate.on('geolocate', function(event) {
 
-})
+
+// https://www.mapbox.com/help/custom-markers-gl-js/
+
+
+
+
+//  // Change the cursor to a pointer when the mouse is over the states layer.
+//  map.on('mouseenter', 'states-layer', function () {
+//      map.getCanvas().style.cursor = 'pointer';
+//  });
+
+//  // Change it back to a pointer when it leaves.
+//  map.on('mouseleave', 'states-layer', function () {
+//      map.getCanvas().style.cursor = '';
+//  });
